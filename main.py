fe564a3adc1daa39ea34ccbd4ee220d59f5c7710
Noelle <73260931+No767@users.noreply.github.com>
2023-05-08 22:13:07 -0700

    Add in a subreddit searching option

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import typer
 import praw
+from dotenv import load_dotenv
+import os
+from typing import Literal
+from typing_extensions import Annotated
+from rich.columns import Columns
+from rich import print
+from rich.table import Table
+from rich.console import Console
+load_dotenv()
 
 app = typer.Typer(rich_markup_mode="rich")
+console = Console()
 
 @app.command()
-def search(subreddit: str):
-    reddit = praw.Reddit()
+def main():
+    print("help")
+@app.command()
+def subreddit(subreddit: str, filter: Annotated[str, typer.Option(help="filters")]):
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_ID"),
+        client_secret=os.getenv("REDDIT_SECRET"),
+        user_agent="testing folks"
+    )
     currSub = reddit.subreddit(subreddit)
-    print(currSub.title)
+    subGen = currSub.new(limit=15) if filter == "new" else currSub.hot(limit=15) if filter == "hot" else currSub.top(limit=15)
+    table = Table()
+    table.add_column("Title")
+    table.add_column("URL")
+    for posts in subGen: table.add_row(posts.title, posts.url)
+    console.print(table)
     
+
 if __name__ == "__main__":
     app()
