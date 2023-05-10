e9d54ba6573ce011d9c1be8af5645bed6d930aa2
Noelle <73260931+No767@users.noreply.github.com>
2023-05-09 22:11:14 -0700

    Add pre-commit hooks

--- commands/subreddit.py
+++ commands/subreddit.py
@@ -1,33 +1,45 @@
-import typer
-import praw
-from dotenv import load_dotenv
 import os
-from typing_extensions import Annotated
-from rich.console import Console
+
+import praw
+import typer
+from dotenv import load_dotenv
 from praw.models import Submission
-from commands.utils import formatSubredditTable
+from rich.console import Console
+from typing_extensions import Annotated
+
+from commands.utils import formatSubredditTable, parseSubreddit
+
 load_dotenv()
 
 app = typer.Typer(rich_markup_mode="rich")
 reddit = praw.Reddit(
-        client_id=os.getenv("REDDIT_ID"),
-        client_secret=os.getenv("REDDIT_SECRET"),
-        user_agent="testing folks"
-    )
+    client_id=os.getenv("REDDIT_ID"),
+    client_secret=os.getenv("REDDIT_SECRET"),
+    user_agent="testing folks",
+)
 console = Console()
+
 
 @app.command()
 def browse(subreddit: str, filter: Annotated[str, typer.Option(help="filters")]):
     currSub = reddit.subreddit(subreddit)
-    subGen: Submission = currSub.new(limit=15) if filter == "new" else currSub.hot(limit=15) if filter == "hot" else currSub.top(limit=15)
+    subGen: Submission = (
+        currSub.new(limit=15)
+        if filter == "new"
+        else currSub.hot(limit=15)
+        if filter == "hot"
+        else currSub.top(limit=15)
+    )
     console.print(formatSubredditTable(subGen))
+
 
 @app.command()
 def search(subreddit: str, search: str):
-    currSub = reddit.subreddit(subreddit).search(search)
+    currSub = reddit.subreddit(parseSubreddit(subreddit)).search(search)
     console.print(formatSubredditTable(currSub))
+
 
 @app.command()
 def random(subreddit: str):
-    currSub = reddit.subreddit(subreddit).random()
+    currSub = reddit.subreddit(parseSubreddit(subreddit)).random()
     console.print(formatSubredditTable(currSub))
--- commands/utils.py
+++ commands/utils.py
@@ -1,6 +1,10 @@
-from rich.table import Table
+import re
 from datetime import datetime
+from typing import Union
+
 from praw.models import Subreddit
+from rich.table import Table
+
 
 def formatSubredditTable(subreddit: Subreddit) -> Table:
     """Formats the given subreddit into a Rich Table
@@ -16,6 +20,28 @@ def formatSubredditTable(subreddit: Subreddit) -> Table:
     table.add_column("Flair")
     table.add_column("URL")
     table.add_column("Date Created")
-    for posts in subreddit: 
-        table.add_row(posts.title, posts.author_flair_text or "None", posts.url, datetime.fromtimestamp(posts.created_utc).strftime("%a %d %b %Y %H:%M:%S"))
+    for posts in subreddit:
+        table.add_row(
+            posts.title,
+            posts.author_flair_text or "None",
+            posts.url,
+            datetime.fromtimestamp(posts.created_utc).strftime("%a %d %b %Y %H:%M:%S"),
+        )
     return table
+
+
+# NOTE: This directly comes from Kumiko. This util function is still my own
+# I am the owner and creator of Kumiko
+# https://github.com/No767/Kumiko/blob/b9cce3efaafc79e1066bde6506523cab3ae9f6f0/Bot/Libs/utils/utils.py#LL48C1-L59C61
+def parseSubreddit(subreddit: Union[str, None]) -> str:
+    """Parses a subreddit name to be used in a reddit url
+
+    Args:
+        subreddit (Union[str, None]): Subreddit name to parse
+
+    Returns:
+        str: Parsed subreddit name
+    """
+    if subreddit is None:
+        return "all"
+    return re.sub(r"^[r/]{2}", "", subreddit, re.IGNORECASE)
